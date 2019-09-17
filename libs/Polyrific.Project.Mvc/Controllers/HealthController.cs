d6875f616583f46bb8a3ad12a2e847ba87e6b43a
Frandi Dwi <dev@rumahfrandi.com>
2019-09-17 11:02:40 +0700

    Add libs

---
+++ libs/Polyrific.Project.Mvc/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Polyrific.Project.Mvc.Controllers
+{
+    [Route("health")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult CheckHealth()
+        {
+            return new OkResult();
+        }
+    }
+}
---
+++ libs/Polyrific.Project.Mvc/MvcBuilderExtension.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Polyrific.Project.Mvc
+{
+    public static class MvcBuilderExtension
+    {
+        /// <summary>
+        /// Add controllers from this base project
+        /// </summary>
+        /// <param name="builder">An <see cref="IMvcBuilder"/> that can be used to further configure the MVC services.</param>
+        /// <returns></returns>
+        public static IMvcBuilder AddBaseProjectControllers(this IMvcBuilder builder)
+        {
+            builder.AddApplicationPart(typeof(MvcBuilderExtension).Assembly);
+
+            return builder;
+        }
+    }
+}
