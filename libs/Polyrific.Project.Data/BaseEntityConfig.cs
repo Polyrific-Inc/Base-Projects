790cffa2ababe6ce9aa731250d3777281cd194c2
Frandi Dwi <frandi.dwijayanto@polyrific.com>
2019-12-17 19:23:13 +0700

    Allow to set table name in entity config

--- libs/Polyrific.Project.Data/BaseEntityConfig.cs
+++ libs/Polyrific.Project.Data/BaseEntityConfig.cs
@@ -11,12 +11,34 @@ namespace Polyrific.Project.Data
     /// <typeparam name="TEntity">The entity to configure</typeparam>
     public abstract class BaseEntityConfig<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity
     {
+        private readonly string _tableName;
+
+        /// <summary>
+        /// Instantiate the Entity Config
+        /// </summary>
+        public BaseEntityConfig()
+        {
+
+        }
+
+        /// <summary>
+        /// Instantiate the Entity Config
+        /// </summary>
+        /// <param name="tableName">The table name of the entity in database</param>
+        public BaseEntityConfig(string tableName)
+        {
+            _tableName = tableName;
+        }
+
         /// <summary>
         /// Configure the property in base entity
         /// </summary>
         /// <param name="builder">The entity type builder instance</param>
         public virtual void Configure(EntityTypeBuilder<TEntity> builder)
         {
+            if (!string.IsNullOrEmpty(_tableName))
+                builder.Metadata.SetTableName(_tableName);
+
             builder.Property(e => e.ConcurrencyStamp).IsConcurrencyToken();
             builder.Property(e => e.Created).HasConversion(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
             builder.Property(e => e.Updated).HasConversion(v => v, v => v != null ? (DateTime?)DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : null);
