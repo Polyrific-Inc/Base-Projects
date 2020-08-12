b213b15574da785d8a8e1b29ed28267a8a33af03
andryrifaldi <andry.rifaldi@gmail.com>
2020-08-12 07:37:55 +0100

    add unit test

--- samples/2020/Core/Services/ProductService.cs
+++ samples/2020/Core/Services/ProductService.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Core.Entities;
 using Microsoft.Extensions.Logging;
@@ -23,6 +25,48 @@ namespace Core.Services
             var items = await _productRepository.GetBySpec(spec);
 
             return items;
+        }
+
+        public async Task<Paging<Product>> GetPageDataWithFilterOperation(int? page = null,
+            int? pageSize = null,
+            string orderBy = null,
+            string filter = null,
+            Op operation = Op.Contains,
+            bool @descending = false)
+        {
+            if (page < 1)
+                page = 1;
+
+            int? skip = (page - 1) * pageSize;
+
+            // Sorting
+            Expression<Func<Product, object>> _orderBy = ExpressionBuilder.GetSortExpression<Product>(orderBy);
+
+            // Filter
+            Expression<Func<Product, bool>> criteria = u => true;
+
+            if (!string.IsNullOrEmpty(filter))
+            {
+                var filters = ExpressionBuilder.BuildFilter(filter, operation);
+
+                criteria = ExpressionBuilder.GetExpression<Product>(filters);
+            }
+
+            var spec = new Specification<Product>(criteria, _orderBy, descending, skip, pageSize);
+
+            try
+            {
+                var items = await Repository.GetBySpec(spec);
+                var total = await Repository.CountBySpec(spec);
+
+                return new Paging<Product>(items, total, page, pageSize);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "Failed to get {pageSize} Product on page {page}", pageSize, page);
+
+                return new Paging<Product>();
+            }
         }
     }
 }
